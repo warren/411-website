7af5391d1fe03da925fa7480a5acd478bef1f92c
Warren-Partridge <maps@perdix.org>
2018-11-16 10:48:36 -0500

    Start adding homepage frontend

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,24 @@ const Section = styled('div')`
     text-align: center;
 `;
 
+const BlackBackground = styled('div')`
+    background-color: #606060
+`;
+
 const App: React.SFC = () => {
     return (
       <>
+        <BlackBackground>
+            <h1>Uber Eats (Out!)</h1>
+
+            <span>
+                <button>Log in with Google</button>
+                <button>Sign Up</button>
+            </span>
+            <h3>*Uber account required (duh)</h3>
+        </BlackBackground>
+
+
         <FeaturedText>Zomato API</FeaturedText>
         <Section>
           <Yelp />
