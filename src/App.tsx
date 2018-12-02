b9f96b1fffd70ba2ca4ed0d30d125016e8d0880a
Ferris Armstrong <fda@bu.edu>
2018-12-01 22:42:40 -0500

    Added Google Login

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import styled from 'react-emotion';
 import { Col } from 'reactstrap';
 import './buttons.css';
-import { Welcome } from './pages';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import { GoogleLogin } from 'react-google-login';
 
 const Section = styled('div')`
     text-align: center;
@@ -33,30 +33,85 @@ const MainHeaderBlue = styled('h1')`
     font-size: 60px;
 `;
 
-const App: React.SFC = () => {
-    return (
-      <Router>
-        <div>
-            <TitleSection>
-                <MainHeaderWhite>Uber</MainHeaderWhite>
-                <MainHeaderGreen>Eats</MainHeaderGreen>
-                <MainHeaderBlue>(Out!)</MainHeaderBlue>
-            </TitleSection>
+const GoogleSignInButton = styled('a')`
+    font-size: 14px;
+    padding: 10px;
+    margin: 30px 0px;
+`;
 
-            <Section>
-                <Col sm="12" md={{ size: 6, offset: 3 }}>
-                    <a href="#"
-                        className="btn btn-sm animated-button thar-three"
-                        onClick={() => { alert('Google button clicked') }}>
-                        Log in with Google
-                    </a>
-                </Col>
-            </Section>
-            <Link to="/pages/welcome/">About</Link>
-            <Route path="/pages/welcome/" component={Welcome} />
-        </div>
-      </Router>
-    );
+interface State {
+    isSignedIn: boolean;
+    name: string;
+    image: string;
 }
 
-export default App;
+export default class App extends React.Component<{}, State> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            image: '',
+            isSignedIn: false,
+            name: '',
+        };
+
+        this.responseSuccess = this.responseSuccess.bind(this);
+    }
+
+    public componentDidMount() {
+        const user = JSON.parse(String(localStorage.getItem('userInfo')));
+        const check = Boolean(user);
+        console.log(user)
+        if (check === false) {
+            return;
+        }
+
+        // const userToken = user.El;
+        const nameObj = user.profileObj.givenName;
+        const imageObj = user.profileObj.imageUrl;
+
+        this.setState({
+            image: imageObj,
+            isSignedIn: check,
+            name: nameObj,
+        });
+    }
+
+    public responseSuccess = (response: any) => {
+        localStorage.setItem('userInfo', JSON.stringify(response));
+        console.log('you got here')
+    }
+
+    public responseFailure = (response: any) => {
+        console.log(response);
+    }
+
+    public render() {
+        return (
+            <>
+                <TitleSection>
+                    <MainHeaderWhite>Uber</MainHeaderWhite>
+                    <MainHeaderGreen>Eats</MainHeaderGreen>
+                    <MainHeaderBlue>(Out!)</MainHeaderBlue>
+                </TitleSection>
+
+                <Section>
+                    <Col sm="12" md={{ size: 6, offset: 3 }}>
+                        <GoogleLogin
+                            clientId=""
+                            render={renderProps => (
+                                <GoogleSignInButton className="btn btn-sm animated-button thar-three" onClick={renderProps.onClick}>LOG IN</GoogleSignInButton>
+                            )}
+                            onSuccess={this.responseSuccess}
+                            onFailure={this.responseFailure}
+                        />
+                    </Col>
+                </Section>
+                {
+                    this.state.isSignedIn ? <Redirect to="/pages/welcome" /> : null
+                }
+            </>
+        );
+    }
+}
+
