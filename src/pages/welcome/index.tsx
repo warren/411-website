2d261818fcbeeefd7d5a44d26bdc82a0127a5d6b
Ferris Armstrong <fda@bu.edu>
2018-12-03 19:17:24 -0500

    Added database

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react';
 import { Zomato } from './components';
 import styled from 'react-emotion';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+
+var config = {
+    apiKey: "AIzaSyB7SgUH7cBnAvzy4GCw6LQl1hfPEGxaRjc",
+    authDomain: "voltaic-mode-193603.firebaseapp.com",
+    databaseURL: "https://voltaic-mode-193603-bf497.firebaseio.com",
+    projectId: "voltaic-mode-193603",
+    storageBucket: "voltaic-mode-193603.appspot.com",
+    messagingSenderId: "356024695903"
+};
 
 const FeaturedText = styled('h1')`
     text-align: center;
@@ -18,16 +29,61 @@ const Container = styled('div')`
     margin: 30px;
 `;
 
-const Welcome: React.SFC<{}> = () => {
-    console.log(window.history.state.state)
-    return (
-        <Container>
-            <FeaturedText>Hello, {window.history.state.state.name}</FeaturedText>
-            <Section>
-                <Zomato />
-            </Section>
-        </Container>
-    );
+const SuggestionText = styled('h3')`
+    color: #FFF;
+`;
+
+interface State {
+    choice: string;
+    errorMsg: string;
 }
 
-export default Welcome;
+export default class Welcome extends React.Component<{}, State> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            choice: '',
+            errorMsg: null
+        };
+    }
+
+
+    public componentDidMount() {
+        firebase.initializeApp(config);
+        const items = firebase.database().ref(window.history.state.state.token);
+        items.on('value', (item) => {
+            const object = item.val();
+            try {
+                this.setState({
+                    choice: object.Choice
+                })
+            } catch {
+                this.setState({
+                    errorMsg: "Sorry we can't find any user data, we'll start tracking now!"
+                })
+
+                //Add User Data to the Firebase
+                const Items = ["Chinese", "Spanish", "Italian"];
+                firebase.database().ref(window.history.state.state.token).set({
+                    Array: Items,
+                    Choice: Items[Math.floor(Math.random() * Items.length)]
+                })
+            }
+        })
+    }
+
+    public render() {
+        return (
+            <Container>
+                <FeaturedText>Hello, {window.history.state.state.name}</FeaturedText>
+                {
+                    this.state.errorMsg ? <SuggestionText>{this.state.errorMsg}</SuggestionText> : <SuggestionText>We suggest eating: {this.state.choice}</SuggestionText> 
+                }
+                <Section>
+                    <Zomato search={this.state.choice}/>
+                </Section>
+            </Container>
+        );
+    }
+}
