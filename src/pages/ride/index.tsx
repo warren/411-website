036b5fe398da1b6997d618cd15f59e906eeaee64
Ferris Armstrong <fda@bu.edu>
2018-12-10 17:09:03 -0500

    Added sms

--- src/pages/ride/index.tsx
+++ src/pages/ride/index.tsx
@@ -11,10 +11,11 @@ const SearchBox = styled('input')`
 `;
 
 const Container = styled('div')`
-    flex-direction: column;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    display: flex;           
+    flex-direction: column;  
+    justify-content: center; 
+    align-items: center;     
+    height: 1000px;
 `;
 
 const Title = styled('h1')`
@@ -31,17 +32,47 @@ const SearchButton = styled('button')`
     font-size: 20px;
 `;
 
+const config = {
+    owner: '',
+    user1: '',
+    user2: '',
+    user3: '',
+}
+
 const handleClick = () => {
-    fetch('http://localhost:8080/api/sendSMS')
+    fetch(`http://localhost:8080/api/sendSMS?url=${window.history.state.state.deepLink}`)
 }
 
 const Ride:React.SFC<{}> = () => {
+
+    const setOwner = (props: any) => {
+        config.owner = props.target.value;
+        console.log(config.owner);
+    }
+
+    const setUser1 = (props: any) => {
+        config.user1 = props.target.value;
+        console.log(config.user1);
+    }
+
+    const setUser2 = (props: any) => {
+        config.user2 = props.target.value;
+        console.log(config.user2);
+    }
+
+    const setUser3 = (props: any) => {
+        config.user3 = props.target.value;
+        console.log(config.user3);
+
+    }
+
     return (
         <Container>
             <Title>Let's Get Your Friends!</Title>
-            <SearchBox type="text"/>
-            <SearchBox type="text"/>
-            <SearchBox type="text"/>
+            <SearchBox type="text" onChange={setOwner} placeholder={'Your Number'} />
+            <SearchBox type="text" onChange={setUser1} placeholder={'Friend 1'} />
+            <SearchBox type="text" onChange={setUser2} placeholder={'Friend 2'}/>
+            <SearchBox type="text" onChange={setUser3} placeholder={'Friend 3'}/>
             <SearchButton onClick={handleClick}>
                 Search
             </SearchButton>
